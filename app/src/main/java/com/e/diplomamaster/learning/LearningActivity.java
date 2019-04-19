97c3bf1a741824b3c0628540f13c0e705fb4d54f
SYVY <sivokon94@gmail.com>
2019-04-19 22:05:20 +0300

    checking

--- app/src/main/java/com/e/diplomamaster/learning/LearningActivity.java
+++ app/src/main/java/com/e/diplomamaster/learning/LearningActivity.java
@@ -13,12 +13,16 @@ import com.e.diplomamaster.R;
 import com.e.diplomamaster.learning.lessons.FirstLesson;
 import com.e.diplomamaster.learning.practice.PracticeLesson;
 import com.e.diplomamaster.learning.practice.SecondPracticeLesson1;
+import com.e.diplomamaster.learning.withExplain.ExerciseWithExplain1;
+import com.e.diplomamaster.learning.withExplain.ExerciseWithExplain2;
 
 public class LearningActivity extends AppCompatActivity {
 
     Intent intent = null;
-    AlertDialog.Builder ad;
+    AlertDialog.Builder ad, ad1;
     Context context;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,10 @@ public class LearningActivity extends AppCompatActivity {
         ad = new AlertDialog.Builder(context);
         ad.setTitle(R.string.practProblem);
         ad.setMessage(R.string.choiseProblem);
+
+        ad1 = new AlertDialog.Builder(context);
+        ad1.setTitle(R.string.practProblem2);
+        ad1.setMessage(R.string.choiseProblem);
 
         ad.setPositiveButton(R.string.problem_2, new DialogInterface.OnClickListener() {
             @Override
@@ -61,6 +69,30 @@ public class LearningActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ad.show();
+            }
+        });
+
+        ad1.setPositiveButton(R.string.problem_2, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                intent = new Intent(LearningActivity.this, ExerciseWithExplain2.class);
+                startActivity(intent);
+            }
+        });
+        ad1.setNegativeButton(R.string.problem_1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                intent = new Intent(LearningActivity.this, ExerciseWithExplain1.class);
+                startActivity(intent);
+            }
+        });
+        ad1.setCancelable(true);
+
+        Button thirdLes3 = findViewById(R.id.lesson_3);
+        thirdLes3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ad1.show();
             }
         });
     }
---
+++ app/src/main/java/com/e/diplomamaster/learning/withExplain/ExerciseWithExplain1.java
@@ -0,0 +1,15 @@
+package com.e.diplomamaster.learning.withExplain;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.e.diplomamaster.R;
+
+public class ExerciseWithExplain1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise1_with_explain);
+    }
+}

c8ca581f02c24a07cf9822fd8e938c804b222448
SYVY <sivokon94@gmail.com>
2019-04-22 15:40:59 +0300

    make animation for task

--- app/src/main/java/com/e/diplomamaster/learning/withExplain/ExerciseWithExplain1.java
+++ app/src/main/java/com/e/diplomamaster/learning/withExplain/ExerciseWithExplain1.java
@@ -2,14 +2,41 @@ package com.e.diplomamaster.learning.withExplain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.e.diplomamaster.R;
 
 public class ExerciseWithExplain1 extends AppCompatActivity {
 
+    Button solution;
+    TextView solExplain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_with_explain1);
+
+        solExplain = findViewById(R.id.solExplain);
+        solution = findViewById(R.id.solution);
+
+        solution.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                solution.setVisibility(View.GONE);
+                solExplain.setVisibility(View.VISIBLE);
+                startAnimation();
+            }
+        });
     }
+
+    private void startAnimation(){
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.myalfa);
+        solExplain.startAnimation(animation);
+    }
+
+
 }

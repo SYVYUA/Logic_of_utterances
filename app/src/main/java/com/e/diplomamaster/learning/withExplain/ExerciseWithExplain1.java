98b961dc84f07ae2fbfbac48073c1fe3ae69d0f8
SYVY <sivokon94@gmail.com>
2019-05-24 15:29:30 +0300

    true or false table with color

--- app/src/main/java/com/e/diplomamaster/learning/withExplain/ExerciseWithExplain1.java
+++ app/src/main/java/com/e/diplomamaster/learning/withExplain/ExerciseWithExplain1.java
@@ -24,22 +24,6 @@ public class ExerciseWithExplain1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_with_explain1);
 
-        trueRow23 = findViewById(R.id.trueRow23);
-        falseRow24 = findViewById(R.id.falseRow24);
-        falseRow25 = findViewById(R.id.falseRow25);
-        falseRow26 = findViewById(R.id.falseRow26);
-        falseRow33 = findViewById(R.id.falseRow33);
-        trueRow34 = findViewById(R.id.trueRow34);
-        falseRow35 = findViewById(R.id.falseRow35);
-        falseRow36 = findViewById(R.id.falseRow36);
-        trueRow43 = findViewById(R.id.trueRow43);
-        falseRow44 = findViewById(R.id.falseRow44);
-        falseRow45 = findViewById(R.id.falseRow45);
-        trueRow46 = findViewById(R.id.trueRow46);
-        trueRow53 = findViewById(R.id.trueRow53);
-        trueRow54 = findViewById(R.id.trueRow54);
-        trueRow55 = findViewById(R.id.trueRow55);
-        trueRow56 = findViewById(R.id.trueRow56);
         nextScreen = findViewById(R.id.nextStepExp1);
         layForTable = findViewById(R.id.layForTable);
 
@@ -71,10 +55,15 @@ public class ExerciseWithExplain1 extends AppCompatActivity {
 
     private void startAnimation(){
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.myalfa);
-        int[] txtView = new int[]{R.id.trueRow23, R.id.falseRow24, R.id.falseRow25, R.id.falseRow26, R.id.falseRow33, R.id.trueRow34, R.id.falseRow35, R.id.falseRow36, R.id.trueRow43, R.id.falseRow44, R.id.falseRow45, R.id.trueRow46, R.id.trueRow53, R.id.trueRow54, R.id.trueRow55, R.id.trueRow56};
-        for(int i = 0; i<txtView.length; i++){
-            TextView txtVW = findViewById(txtView[i]);
-            txtVW.setBackgroundColor(Color.GREEN);
+        int[] txtViewGreen = new int[]{R.id.trueRow23, R.id.falseRow24, R.id.falseRow25, R.id.falseRow26, R.id.falseRow33, R.id.trueRow34, R.id.falseRow35, R.id.falseRow36, R.id.trueRow43, R.id.falseRow44, R.id.falseRow45, R.id.trueRow46, R.id.trueRow53, R.id.trueRow54, R.id.trueRow55, R.id.trueRow56};
+        for(int i = 0; i<txtViewGreen.length; i++){
+            TextView txtVWGreen = findViewById(txtViewGreen[i]);
+            txtVWGreen.setBackgroundColor(Color.GREEN);
+        }
+        int[] txtViewRed = new int[]{R.id.falseRow23, R.id.trueRow24, R.id.trueRow25, R.id.trueRow26, R.id.trueRow33, R.id.falseRow34, R.id.trueRow35, R.id.trueRow36, R.id.falseRow43, R.id.trueRow44, R.id.trueRow45, R.id.falseRow46, R.id.falseRow53, R.id.falseRow54, R.id.falseRow55, R.id.falseRow56};
+        for(int i =0; i<txtViewRed.length; i++){
+            TextView txtVWRed = findViewById(txtViewRed[i]);
+            txtVWRed.setBackgroundColor(Color.RED);
         }
         solExplain.startAnimation(animation);
         layForTable.startAnimation(animation);

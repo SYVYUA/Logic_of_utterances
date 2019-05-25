afbff05c01ba0fc7728a14ff91fe9cbac0ac5038
SYVY <sivokon94@gmail.com>
2019-05-25 17:41:01 +0300

    anim for method 1

--- app/src/main/java/com/e/diplomamaster/learning/withExplain/ExerciseWithExplain1.java
+++ app/src/main/java/com/e/diplomamaster/learning/withExplain/ExerciseWithExplain1.java
@@ -13,16 +13,26 @@ import android.widget.TextView;
 
 import com.e.diplomamaster.R;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ExerciseWithExplain1 extends AppCompatActivity {
 
     private Button solution;
-    private TextView explainForTable, solExplain, nextScreen, trueRow23, falseRow24, falseRow25, falseRow26, falseRow33, trueRow34, falseRow35, falseRow36, trueRow43, falseRow44, falseRow45, trueRow46, trueRow53, trueRow54, trueRow55, trueRow56;
+    private TextView txtVWRed, explainForTable, solExplain, nextScreen, trueRow23, falseRow24, falseRow25, falseRow26, falseRow33, trueRow34, falseRow35, falseRow36, trueRow43, falseRow44, falseRow45, trueRow46, trueRow53, trueRow54, trueRow55, trueRow56;
     LinearLayout layForTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_with_explain1);
+
+        int[] txtViewRed = new int[]{R.id.falseRow23, R.id.trueRow24, R.id.trueRow25, R.id.trueRow26, R.id.trueRow33, R.id.falseRow34, R.id.trueRow35, R.id.trueRow36, R.id.falseRow43, R.id.trueRow44, R.id.trueRow45, R.id.falseRow46, R.id.falseRow53, R.id.falseRow54, R.id.falseRow55, R.id.falseRow56};
+        for(int i =0; i<txtViewRed.length; i++){
+            TextView txtVWRed = findViewById(txtViewRed[i]);
+
+        }
 
         nextScreen = findViewById(R.id.nextStepExp1);
         layForTable = findViewById(R.id.layForTable);
@@ -43,6 +53,7 @@ public class ExerciseWithExplain1 extends AppCompatActivity {
                 }
         });
 
+
         nextScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,20 +67,35 @@ public class ExerciseWithExplain1 extends AppCompatActivity {
 
     private void startAnimation(){
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.myalfa);
-        int[] txtViewGreen = new int[]{R.id.trueRow23, R.id.falseRow24, R.id.falseRow25, R.id.falseRow26, R.id.falseRow33, R.id.trueRow34, R.id.falseRow35, R.id.falseRow36, R.id.trueRow43, R.id.falseRow44, R.id.falseRow45, R.id.trueRow46, R.id.trueRow53, R.id.trueRow54, R.id.trueRow55, R.id.trueRow56};
-        for(int i = 0; i<txtViewGreen.length; i++){
-            TextView txtVWGreen = findViewById(txtViewGreen[i]);
-            txtVWGreen.setBackgroundColor(Color.GREEN);
-        }
-        int[] txtViewRed = new int[]{R.id.falseRow23, R.id.trueRow24, R.id.trueRow25, R.id.trueRow26, R.id.trueRow33, R.id.falseRow34, R.id.trueRow35, R.id.trueRow36, R.id.falseRow43, R.id.trueRow44, R.id.trueRow45, R.id.falseRow46, R.id.falseRow53, R.id.falseRow54, R.id.falseRow55, R.id.falseRow56};
-        for(int i =0; i<txtViewRed.length; i++){
-            TextView txtVWRed = findViewById(txtViewRed[i]);
-            txtVWRed.setBackgroundColor(Color.RED);
-        }
         solExplain.startAnimation(animation);
         layForTable.startAnimation(animation);
         Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.zoomin);
         explainForTable.startAnimation(animation1);
+
+        /*Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        },1000);*/
+
+        int[] txtViewGreen = new int[]{R.id.trueRow23, R.id.falseRow24, R.id.falseRow25, R.id.falseRow26, R.id.falseRow33, R.id.trueRow34, R.id.falseRow35, R.id.falseRow36, R.id.trueRow43, R.id.falseRow44, R.id.falseRow45, R.id.trueRow46, R.id.trueRow53, R.id.trueRow54, R.id.trueRow55, R.id.trueRow56};
+        for (int i1 : txtViewGreen) {
+            TextView txtVWGreen = findViewById(i1);
+            txtVWGreen.startAnimation(animation1);
+            txtVWGreen.setBackgroundColor(Color.GREEN);
+            int[] txtViewRed = new int[]{R.id.falseRow23, R.id.trueRow24, R.id.trueRow25, R.id.trueRow26, R.id.trueRow33, R.id.falseRow34, R.id.trueRow35, R.id.trueRow36, R.id.falseRow43, R.id.trueRow44, R.id.trueRow45, R.id.falseRow46, R.id.falseRow53, R.id.falseRow54, R.id.falseRow55, R.id.falseRow56};
+            for (int i2 : txtViewRed) {
+                txtVWRed = findViewById(i2);
+                txtVWRed.startAnimation(animation1);
+                txtVWRed.setBackgroundColor(Color.RED);
+            }
+        }
+    }
+
+    private void update() {
+
 
     }
 }
--- app/src/main/java/com/e/diplomamaster/learning/withExplain/ExerciseWithExplain1_2.java
+++ app/src/main/java/com/e/diplomamaster/learning/withExplain/ExerciseWithExplain1_2.java
@@ -4,16 +4,40 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.e.diplomamaster.R;
 
 public class ExerciseWithExplain1_2 extends AppCompatActivity {
 
+    TextView trueRow23, falseRow23, trueRow33, falseRow33, trueRow43, falseRow43, trueRow53, falseRow53;
+    Button solution, nextStep;
+    LinearLayout layWithTable2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_with_explain1_2);
 
+        solution = findViewById(R.id.butForTable2);
+        layWithTable2 = findViewById(R.id.layForTable2);
+        solution.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layWithTable2.setVisibility(View.VISIBLE);
+                solution.setVisibility(View.GONE);
+                startAnimation();
+            }
+        });
     }
+    private void startAnimation(){
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.sample_anim);
+        layWithTable2.startAnimation(animation);
+
+    }
+
 }

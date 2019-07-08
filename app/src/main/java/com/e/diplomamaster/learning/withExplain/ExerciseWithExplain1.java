298b9984fcbd4f03101ddf9103461dfd2e1ac34b
SYVY <sivokon94@gmail.com>
2019-07-08 14:00:36 +0300

    animation with question mark

--- app/src/main/java/com/e/diplomamaster/learning/withExplain/ExerciseWithExplain1.java
+++ app/src/main/java/com/e/diplomamaster/learning/withExplain/ExerciseWithExplain1.java
@@ -18,7 +18,7 @@ import com.e.diplomamaster.R;
 public class ExerciseWithExplain1 extends AppCompatActivity {
 
     private Button solution;
-    private TextView txtVWRed, explainForTable, solExplain, nextScreen, trueRow23, falseRow23,falseRow24, falseRow25, falseRow26, falseRow33, trueRow34, falseRow35, falseRow36, trueRow43, falseRow44, falseRow45, trueRow46, trueRow53, trueRow54, trueRow55, trueRow56;
+    private TextView txtVWRed, explainForTable, solExplain, nextScreen, falseRow53, falseRow54, falseRow55, falseRow56, falseRow46, trueRow45, trueRow44, falseRow43, trueRow36, trueRow35, falseRow34, trueRow33, trueRow23, falseRow23, trueRow25, trueRow24, falseRow24, falseRow25, falseRow26, falseRow33, trueRow34, falseRow35, falseRow36, trueRow43, falseRow44, falseRow45, trueRow46, trueRow53, trueRow54, trueRow55, trueRow56, trueRow26;
     LinearLayout layForTable;
 
     AnimationDrawable animationDrawable;
@@ -30,14 +30,281 @@ public class ExerciseWithExplain1 extends AppCompatActivity {
         setContentView(R.layout.activity_exercise_with_explain1);
         trueRow23 = findViewById(R.id.trueRow23);
         falseRow23 = findViewById(R.id.falseRow23);
+        falseRow24 = findViewById(R.id.falseRow24);
+        trueRow24 = findViewById(R.id.trueRow24);
+        trueRow25 = findViewById(R.id.trueRow25);
+        falseRow25 = findViewById(R.id.falseRow25);
+        trueRow26 = findViewById(R.id.trueRow26);
+        falseRow26 = findViewById(R.id.falseRow26);
+        falseRow33 = findViewById(R.id.falseRow33);
+        trueRow33 = findViewById(R.id.trueRow33);
+        trueRow34 = findViewById(R.id.trueRow34);
+        falseRow34 = findViewById(R.id.falseRow34);
+        falseRow35 = findViewById(R.id.falseRow35);
+        trueRow35 = findViewById(R.id.trueRow35);
+        falseRow36 = findViewById(R.id.falseRow36);
+        trueRow36 = findViewById(R.id.trueRow36);
+        trueRow43 = findViewById(R.id.trueRow43);
+        falseRow44 = findViewById(R.id.falseRow44);
+        falseRow45 = findViewById(R.id.falseRow45);
+        falseRow43 = findViewById(R.id.falseRow43);
+        trueRow44 = findViewById(R.id.trueRow44);
+        trueRow45 = findViewById(R.id.trueRow45);
+        trueRow46 = findViewById(R.id.trueRow46);
+        falseRow46 = findViewById(R.id.falseRow46);
+        trueRow53 = findViewById(R.id.trueRow53);
+        trueRow54 = findViewById(R.id.trueRow54);
+        trueRow55 = findViewById(R.id.trueRow55);
+        trueRow56 =findViewById(R.id.trueRow56);
+        falseRow53 = findViewById(R.id.falseRow53);
+        falseRow54 = findViewById(R.id.falseRow54);
+        falseRow55 = findViewById(R.id.falseRow55);
+        falseRow56 =findViewById(R.id.falseRow56);
+
+
         animImage = findViewById(R.id.animImage);
         animImage.setBackgroundResource(R.drawable.question_mark);
         animationDrawable = (AnimationDrawable) animImage.getBackground();
 
-        int[] txtViewRed = new int[]{R.id.falseRow23, R.id.trueRow24, R.id.trueRow25, R.id.trueRow26, R.id.trueRow33, R.id.falseRow34, R.id.trueRow35, R.id.trueRow36, R.id.falseRow43, R.id.trueRow44, R.id.trueRow45, R.id.falseRow46, R.id.falseRow53, R.id.falseRow54, R.id.falseRow55, R.id.falseRow56};
-        for(int i =0; i<txtViewRed.length; i++){
-            TextView txtVWRed = findViewById(txtViewRed[i]);
-        }
+        falseRow56.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow56.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow55.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow55.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow54.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow54.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow53.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow53.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow46.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow46.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow45.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow45.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow44.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow44.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow43.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow43.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow36.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow36.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow35.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow35.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow34.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow34.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow33.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow33.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow26.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow26.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow25.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow25.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        falseRow24.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
+
+        trueRow24.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animImage.setVisibility(View.VISIBLE);
+                animationDrawable.start();
+            }
+        });
 
         falseRow23.setOnClickListener(new View.OnClickListener() {
             @Override

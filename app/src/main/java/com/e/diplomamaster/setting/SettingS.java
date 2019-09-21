ba9355401f6f1d5f34964a0e97aece8766b5ac1c
SYVY <sivokon94@gmail.com>
2019-09-21 08:42:53 +0300

    but for language

--- app/src/main/java/com/e/diplomamaster/setting/SettingS.java
+++ app/src/main/java/com/e/diplomamaster/setting/SettingS.java
@@ -1,19 +1,22 @@
 package com.e.diplomamaster.setting;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.Button;
 
 import com.e.diplomamaster.R;
 
 import java.util.Locale;
 
-public class SettingS extends AppCompatActivity {
+public class SettingS extends AppCompatActivity implements View.OnClickListener {
 
     private Button butUaLang, butEngLang;
-    private Locale locale;
+    private Locale myLocale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,28 +24,14 @@ public class SettingS extends AppCompatActivity {
         setContentView(R.layout.activity_setting_s);
 
         //В завершение совместим всю работу со сменой языка в методе onCreate() нашей Activity:
-        this.txt_hello = (TextView)findViewById(R.id.txt_hello);
-        this.btn_en = (Button)findViewById(R.id.btn_en);
-        this.btn_ru = (Button)findViewById(R.id.btn_ru);
-        this.btn_fr = (Button)findViewById(R.id.btn_fr);
-        this.btn_de = (Button)findViewById(R.id.btn_de);
+        butEngLang = findViewById(R.id.changeLang2);
+        butUaLang = findViewById(R.id.changeLang1);
 
-        this.btn_en.setOnClickListener(this);
-        this.btn_ru.setOnClickListener(this);
-        this.btn_fr.setOnClickListener(this);
-        this.btn_de.setOnClickListener(this);
+        butEngLang.setOnClickListener(this);
+        butUaLang.setOnClickListener(this);
 
         loadLocale();
-    }
-
-        /*Resources res = context.getResources();
-// Change locale settings in the app.
-        DisplayMetrics dm = res.getDisplayMetrics();
-        android.content.res.Configuration conf = res.getConfiguration();
-        conf.setLocale(new Locale(language_code.toLowerCase())); // API 17+ only.
-// Use conf.locale = new Locale(...) if targeting lower versions
-        res.updateConfiguration(conf, dm);*/
-    }
+}
 
     //Смена языка в приложении:
     public void changeLang(String lang)
@@ -65,7 +54,7 @@ public class SettingS extends AppCompatActivity {
         SharedPreferences prefs = getSharedPreferences("CommonPrefs", Activity.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString(langPref, lang);
-        editor.commit();
+        editor.apply();
     }
 
     //Загрузка ранее сохраненной локали:
@@ -80,28 +69,20 @@ public class SettingS extends AppCompatActivity {
    // Обновление UI элементов текущего окна (обновлять необходимо только окно, в котором происходит смена локали):
     private void updateTexts()
     {
-        txt_hello.setText(R.string.hello_world);
-        btn_en.setText(R.string.btn_en);
-        btn_ru.setText(R.string.btn_ru);
-        btn_fr.setText(R.string.btn_fr);
-        btn_de.setText(R.string.btn_de);
+
+        butEngLang.setText(R.string.engLang);
+        butUaLang.setText(R.string.uaLang);
     }
 
     //Добавим события на нажатие кнопок. Для этого имплементируем интерфейс OnClickListener для нашей Activity (implements OnClickListener). И реализуем метод onClick().
     public void onClick(View v) {
-        String lang = "en";
+        String lang = "";
         switch (v.getId()) {
-            case R.id.btn_en:
+            case R.id.changeLang2:
                 lang = "en";
                 break;
-            case R.id.btn_ru:
+            case R.id.changeLang1:
                 lang = "ru";
-                break;
-            case R.id.btn_de:
-                lang = "de";
-                break;
-            case R.id.btn_fr:
-                lang = "fr";
                 break;
             default:
                 break;
